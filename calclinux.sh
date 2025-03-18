fcb7a3329565dde60672c8f0a52b2ca88a88144d
luanavieiralvs <luana_vieira@hotmail.com>
2025-03-18 14:48:38 -0300

    Create calclinux.sh
    Adicionando script linux

---
+++ calclinux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script para executar a calculadora em Python
+
+# Atualização e instalação do Python:
+sudo apt update
+sudo apt install python3
+
+# Caminho para calculadora em Python:
+
+python3 /home/luanalvs/modulo1/python/projeto_calculadora.py
